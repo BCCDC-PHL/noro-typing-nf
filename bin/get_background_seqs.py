0c5598bc5e0990f29b8b9ed4471d6a1169830b57
jpalmer37 <john.palmer1288@gmail.com>
2023-06-19 11:57:17 -0700

    made get_background_seqs.py knowledgeable of current run names

--- bin/get_background_seqs.py
+++ bin/get_background_seqs.py
@@ -10,22 +10,30 @@ from datetime import datetime as dt
 def init_parser():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('path', help='')
+	parser.add_argument('-f', '--infasta', required=True, help='Multifasta file of current run sequences.')
 	parser.add_argument('-s', '--schema', default='results*/phylo/{gene}/align/*.multi.fasta', help='Globbing path schema (with wildcards) used to search for consensus sequences. Use {gene} to indicate the variable gene name.')
 	parser.add_argument('-g', '--gene', default='full', help='Type of sequences: 1) "full" genome 2) "capsid" gene or 3) "polymerase" gene')
 	parser.add_argument('-o', '--outfasta', default='background_seqs.fasta', help='Name of the output FASTA file containing the background sequences')
+	parser.add_argument('-m', '--maxseqs', default=50, type=int, help='Name of the output FASTA file containing the background sequences')
 	return parser
 
 def get_create_time(path):
 	t = os.stat(path).st_ctime
 	return dt.fromtimestamp(t)
 
-def read_sequences(path):
-	return [x for x in SeqIO.parse(path, 'fasta') if x.id.endswith('sample')]
+def read_sequences(path, target_set):
 
-def sample_sequences(path, n):
-	seqs = read_sequences(path)
-	if n > len(seqs):
+	return [x for x in SeqIO.parse(path, 'fasta') if x.id.endswith('sample') and x.id.split("|")[0] not in target_set]
+
+def sample_sequences(path, n, target_set):
+	print(path, n)
+	if n == 0:
 		return []
+
+	seqs = read_sequences(path, target_set)
+	print(seqs)
+	if n > len(seqs):
+		return seqs
 	else:
 		return random.sample(seqs, n) 
 
@@ -33,6 +41,8 @@ def main():
 	parser = init_parser()
 	args = parser.parse_args()
 
+	sample_ids = {x.id.split("|")[0] for x in SeqIO.parse(args.infasta, 'fasta') if 'sample' in x.id}
+	print(sample_ids)
 	# parse the file path schema
 	schema = re.sub("\{.+\}", args.gene+"*", args.schema)
 
@@ -40,16 +50,13 @@ def main():
 	consensus_files = glob(os.path.join(args.path, schema))
 
 	# count the number of sequences per file 
-	counts = list(map(lambda x: len(read_sequences(x)), consensus_files))
+	counts = list(map(lambda x: len(read_sequences(x, sample_ids)), consensus_files))
 
-	max_sequences = 50
-	min_sequences = 1
-	
-	# if number of run folders exceeds N max sequences, downsample to most recent N runs 
-	if len(consensus_files) > max_sequences:
-		create_times = [(path, get_create_time(path)) for path in consensus_files]
-		create_times = sorted(create_times, key=lambda x: x[1], reverse=True)
-		consensus_files = [x[0] for x in create_times[0:max_sequences]]
+	max_sequences = args.maxseqs
+	min_sequences = 0
+
+	print(sum(counts))
+	print(max_sequences)
 
 	# downsampling 
 	if sum(counts) > max_sequences:
@@ -58,22 +65,25 @@ def main():
 
 		# calculate proportion sample sizes 
 		sample_counts = [int(x * proportion) for x in counts]
-		sample_counts = [x if x >= min_sequences else min_sequences for x in sample_counts]
+		# sample_counts = [x if x >= min_sequences else min_sequences for x in sample_counts]
 
 		print("Original: ", sum(counts))
 		print("Sampled: ", sum(sample_counts))
 
 		# down sample to appropriate counts per file 
-		seqs = [seq for path, n in zip(consensus_files, sample_counts) for seq in sample_sequences(path, n)]
+		seqs = [seq for path, n in zip(consensus_files, sample_counts) for seq in sample_sequences(path, n, sample_ids)]
 	
 	# no sampling required
 	else:
 		# simply extract all sequences
-		seqs = [seq for path in consensus_files for seq in SeqIO.parse(path, 'fasta')]
+		seqs = [seq for path in consensus_files for seq in SeqIO.parse(path, 'fasta') if "sample" in seq.id and seq.id.split('|')[0] not in sample_ids]
 
 	# rename sequences for background formatting
 	for s in seqs:
-		s.id = s.id.replace("sample", "background")
+		newheader = s.id.replace("sample", "background|"+dt.today().strftime('%Y-%m-%d'))
+		s.id = newheader
+		s.name = newheader
+		s.description = newheader
 
 	# write outputs 
 	SeqIO.write(seqs, args.outfasta, 'fasta')
