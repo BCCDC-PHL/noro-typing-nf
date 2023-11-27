a5b2a7100f235f15a030c4ff81968b1f12a9b59b
jpalmer37 <john.palmer1288@gmail.com>
2023-11-27 11:28:14 -0800

    updated context tree

--- bin/build_context_tree.R
+++ bin/build_context_tree.R
@@ -17,15 +17,13 @@ if (length(args) == 2){
   delim = args[3]
 }
 
-delim = '|'
+delim = '\\|'
+
 # 1: input tree
 # 2: metadata file indicating target / background 
 # 3: output PDF plot 
 
-# setwd("/Users/jpalmer/work/norovirus/references/G9-test")
-
 tree = read.tree(args[1])
-# tree = read.tree('~/work/norovirus/results-jan-31-9/phylo/capsid/tree/jan-31-9-gtype.nwk')
 
 reference_mask = str_count(tree$tip.label, delim) == 2
 sample_mask = (str_count(tree$tip.label, delim) == 4) & (sapply(tree$tip.label, function(x) str_split(x, delim)[[1]][4] == 'sample'))
@@ -41,13 +39,13 @@ tree <- groupOTU(tree,branches)
 
 print("ggtree")
 p <- ggtree(tree, aes(color=group)) +
-  geom_tiplab(align=T, linesize=0.5) + xlim(NA,2.5) +   
+  geom_tiplab(align=T, linesize=0.5) + xlim(NA,3.1) +   
   scale_color_manual(name="Sequence Type", values=c(Background = "darkblue", Samples= "red", References="darkgray")) + 
   guides(color = guide_legend(override.aes = list(size = 5))) 
 
 print("ggsave")
 # pdf(file = args[2], width = 12, height = 8)
 # plot(p)
-ggsave(args[2], device="pdf")
+ggsave(args[2], width=9, height=6 + 0.135 * Ntip(tree), device="pdf", limitsize=F)
 # dev.off()
 
